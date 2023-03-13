87e24b05a64cf0fd983b2d1948b0b0e674758df3
s21242 <s21242@pjwstk.edu.pl>
2023-03-13 19:17:20 +0100

    initial Commit

---
+++ BMI/app/src/main/java/com/example/tipper/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.tipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable; // for EditText event handling
+import android.text.TextWatcher; // EditText listener
+import android.widget.EditText; // for bill amount input
+import android.widget.SeekBar; // for changing the tip percentage
+import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
+import android.widget.TextView; // for displaying text
+
+import java.text.NumberFormat; // for currency formatting
+
+public class MainActivity extends AppCompatActivity {
+
+    private double weight = 0.0;
+    private double height = 0.15;
+    //TextViews definitions
+    private TextView heightTextView;
+    private TextView weightTextView;
+
+
+    private TextView totalTextView;
+
+    //method onCreate, most important one
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call superclass onCreate
+        setContentView(R.layout.activity_main); // inflate the GUI
+
+//looking up TextViews in layout
+        weightTextView = (TextView) findViewById(R.id.weightTextView);
+        heightTextView = (TextView) findViewById(R.id.heightTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        totalTextView.setText(String.format(0 + ""));
+
+// setting TextWatchers for both weight and height fields
+        EditText weightEditText =
+                (EditText) findViewById(R.id.weightEditText);
+        weightEditText.addTextChangedListener(weightEditTextWatcher);
+        EditText heightEditText =
+                (EditText) findViewById(R.id.heightEditText);
+        heightEditText.addTextChangedListener(heightEditTextWatcher);
+
+    }
+
+    //method calculate, calculating BMI, setting totalTextView as result
+    private void calculate() {
+
+
+        double total = weight / (height * height);
+
+
+        totalTextView.setText(String.format(total + ""));
+    }
+
+
+    // listeners object for TextViews weight and height text-changed events
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
+        // called when the user modifies weight or height field, or both
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                weight = Double.parseDouble(s.toString()) / 100.0;
+                weightTextView.setText(String.format(weight + ""));
+            } catch (NumberFormatException e) {
+                weightTextView.setText("");
+                weight = 0.0;
+            }
+//recalculate on change
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                height = Double.parseDouble(s.toString()) / 100.0;
+                heightTextView.setText(String.format(height + ""));
+            } catch (NumberFormatException e) {
+                heightTextView.setText("");
+                height = 0.0;
+            }
+//recalculate on change
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
+}
+
+
+/*************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
